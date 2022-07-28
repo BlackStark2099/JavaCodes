5106c45b68cce56992d1e035c5021ba070191e02
manan mishra <manan.mishra@nagarro.com>
2022-07-28 23:13:54 +0530

    added normal stack

--- JavaCodes/stack_and_queue/normal_stack.java
+++ JavaCodes/stack_and_queue/normal_stack.java
@@ -15,23 +15,44 @@ public class normal_stack{
     }
 
     int size() {
-      // write ur code here
+      return tos+1;
     }
 
     void display() {
-      // write ur code here
+      for(int i = tos;i>=0;i--){
+        System.out.print(data[i]+" ");
+      }
+      System.out.println();
     }
 
     void push(int val) {
-      // write ur code here
+      if(tos == data.length-1){
+        System.out.println("Stack overflow");
+        return ;
+      }else{
+        tos++;
+        data[tos] = val;
+      }
     }
 
     int pop() {
-      // write ur code here
+      if(tos == -1){
+        System.out.println("Stack underflow");
+        return -1;
+      }else{
+        tos--;
+        return data[tos+1];
+      }
     }
 
     int top() {
        // write ur code here
+       if(tos == -1){
+        System.out.println("Stack underflow");
+        return -1;
+       }else{
+        return data[tos];
+       }
     }
   }
 
