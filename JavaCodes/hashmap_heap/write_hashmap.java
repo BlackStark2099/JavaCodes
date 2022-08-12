2f0a8b79f69b8ecaf1758f1440c9fa474526a904
BlackStark2099 <manan9540608841@gmail.com>
2022-08-12 23:21:05 +0530

    added hash map

--- JavaCodes/hashmap_heap/write_hashmap.java
+++ JavaCodes/hashmap_heap/write_hashmap.java
@@ -32,27 +32,75 @@ public class write_hashmap{
     }
 
     public void put(K key, V value) throws Exception {
-      // write your code here
+      int bi = hashFunction(key);
+      size++;
+      buckets[bi].add(new HMNode(key, value));
+      return;
     }
 
     public V get(K key) throws Exception {
-      // write your code here
+      int bi = hashFunction(key);
+      int di = findInBucket(key, bi);
+      if(di == -1){
+        return null;
+      }else{
+        HMNode pair = buckets[bi].get(di);
+        return pair.value;
+      }
     }
 
     public boolean containsKey(K key) {
-      // write your code here
+      int bi = hashFunction(key);
+      int di = findInBucket(key,bi);
+      if(di == -1){
+        return false;
+      }else{
+        return true;
+      } 
     }
 
     public V remove(K key) throws Exception {
+      int bi = hashFunction(key);
+      int di = findInBucket(key, bi);
+      if(di == -1){
+        return null;
+      }else{
+        HMNode pair = buckets[bi].get(di);
+        buckets[bi].remove(di);
+        size--;
+        return pair.value;
+      }
       // write your code here
     }
 
     public ArrayList<K> keyset() throws Exception {
+      ArrayList<K> keys = new ArrayList<>();
+      for(int i=0;i<buckets.length;i++){
+       LinkedList<HMNode> lList = buckets[i];
+       HMNode temp = lList.get(0);
+       keys.add(temp.key);
+      }
+      return keys;
       // write your code here
     }
 
     public int size() {
       // write your code here
+      return size;
+    }
+
+    public int hashFunction(K key){
+      return 
+    }
+
+    public int findInBucket(K key, int bi){
+      for(int di = 0; di < buckets[bi].size();di++){
+        HMNode pair = buckets[bi].get(di);
+        if(pair.key.equals(key)){
+          return di;
+        }
+      }
+      return -1;
     }
 
    public void display() {
