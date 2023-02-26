be09330bec11ff194f97b5bde00b760286b88d85
Manan Mishra <manan.mishra@nagarro.com>
2023-02-26 20:41:06 +0530

    solved Paint House - many colours pep

---
+++ JavaCodes/dynamic_programming/paint_house_many_colours.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class paint_house_many_colours {
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String num = br.readLine();
+        String[] numItems = num.split(" ");
+        int n = Integer.parseInt(numItems[0]);
+        int m = Integer.parseInt(numItems[1]);
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            String[] items = str.split(" ");
+            for(int j=0;j<m;j++){
+                arr[i][j] = Integer.parseInt(items[j]);
+            }
+        }
+        int [][] dp = new int [n][m]; 
+        for(int i=0;i<m;i++){
+            dp[0][i] = arr[0][i];
+        }
+    
+        for(int p=1;p<n;p++){
+            for(int q=0;q<m;q++){
+                int min = Integer.MAX_VALUE;
+                for(int k=0;k<m;k++){
+                    if(q==k){
+                        continue;
+                    }
+                    min = Math.min(min, dp[p-1][k]);
+                }
+                dp[p][q] = min+arr[p][q];
+            }
+        }
+        int min = Integer.MAX_VALUE;
+        for(int i=0;i<m;i++){
+            min = Math.min(min,dp[n-1][i]);
+        }
+        System.out.println(min);
+    }
+}
