f149272b4d221dc5254bb5da2c827034887fb458
Manan Mishra <manan.mishra@nagarro.com>
2023-03-02 14:17:36 +0530

    solved count_of_subarrays_having_sum_equals_to_k pep

---
+++ LeetCode/subarray_sum_equals_k_560.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0,1);
+        int sum = 0; 
+        int res = 0;
+        for(int i=0;i<nums.length;i++){
+            sum = sum + nums[i];
+            if(map.get(sum-k) != null){
+                res = res + map.get(sum-k);
+            }
+            if(map.get(sum) == null){
+                map.put(sum,1);
+            }else{
+                map.put(sum,map.get(sum)+1);
+            }
+        }
+        return res;
+    }
+}
