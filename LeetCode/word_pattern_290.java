2cd2feb59bdc05d99c4f135bd8d7aafd8afdce63
BlackStark2099 <manan9540608841@gmail.com>
2023-03-17 17:55:39 +0530

    solved word_pattern_290 leetcode

---
+++ LeetCode/word_pattern_290.java
@@ -0,0 +1,30 @@
+package LeetCode;
+import java.util.*;
+
+class Solution {
+    public boolean wordPattern(String s, String t) {
+        HashMap<Character,String> map = new HashMap<>();
+        HashSet<String> set = new HashSet<>();
+        String [] stArr = t.split(" ");
+        if(s.length() != stArr.length) return false;
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(!map.get(s.charAt(i)).equals(stArr[i])){
+                    // System.out.println(map+" "+s.charAt(i)+" "+stArr[i]+" "+map.get(s.charAt(i)).equals(stArr[i]));
+                    // System.out.println("hel");
+                    return false;
+                }
+            }else{
+                if(set.contains(stArr[i])) {
+                // System.out.println("helo");
+                    
+                    return false;
+                
+                }map.put(s.charAt(i),stArr[i]);
+                set.add(stArr[i]);
+            }
+            
+        }
+        return true;
+    }
+}
